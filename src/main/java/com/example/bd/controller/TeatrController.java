6ce223e9de35383f226980b795596c197b6ffdc8
Yulia <kjd2001@yandex.ru>
2021-04-22 21:13:52 +0300

    Методы post везде работают

--- src/main/java/com/example/bd/controller/TeatrController.java
+++ src/main/java/com/example/bd/controller/TeatrController.java
@@ -5,9 +5,13 @@ import com.example.bd.repo.ActorRepo;
 import com.example.bd.repo.RaspisanieRepo;
 import com.example.bd.repo.TeatrRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 public class TeatrController {
     @Autowired
@@ -15,6 +19,47 @@ public class TeatrController {
     @Autowired
     private RaspisanieRepo raspisanieRepo;
 
+    @PostMapping(value = "/teatrs")
+    public Teatr create(@RequestBody Teatr teatr) {
+        return teatrRepo.save(teatr);
+    }
+
+    @GetMapping(value = "/teatrs")
+    public ResponseEntity<List<Teatr>> read() {
+        List<Teatr> teatr = teatrRepo.findAll();
+        return !teatr.isEmpty()
+                ? new ResponseEntity<>(teatr, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/teatrs/{id}")
+    public ResponseEntity<Teatr> read(@PathVariable(name = "id") Long id) {
+        Teatr teatr = teatrRepo.findById(id).orElse(null);
+
+        return teatr != null
+                ? new ResponseEntity<>(teatr, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(value = "/teatrs/{id}")
+    public Teatr update(@PathVariable(name = "id") Long teatrId, @RequestBody Teatr teatrReq) {
+        return teatrRepo.findById(teatrId).map(
+                teatr -> {
+                    teatr = teatrReq;
+                    return teatrRepo.save(teatr);
+                }
+        ).orElseThrow(() -> new ResourceNotFoundException("Person not found with id" + teatrId));
+    }
+
+    @DeleteMapping(value = "/teatrs/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") Long teatrId) {
+        return teatrRepo.findById(teatrId)
+                .map(teatr -> {
+                    teatrRepo.delete(teatr);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new ResourceNotFoundException("Person not found with id" + teatrId));
+    }
+/*
     @PostMapping("/teatrs")
     public void create(@RequestBody Teatr teatr) {
         teatrRepo.save(teatr);
@@ -49,6 +94,8 @@ public class TeatrController {
                 ? new ResponseEntity<>(teatr, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+ */
 
 
 }
