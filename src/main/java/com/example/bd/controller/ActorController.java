28d6671c5cc1b09487a7f312499771f44b2ffa47
Yulia <kjd2001@yandex.ru>
2021-04-23 18:07:26 +0300

    Работает все для актера

--- src/main/java/com/example/bd/controller/ActorController.java
+++ src/main/java/com/example/bd/controller/ActorController.java
@@ -1,25 +1,42 @@
 package com.example.bd.controller;
 
 import com.example.bd.model.Actor;
+import com.example.bd.model.Teatr;
 import com.example.bd.repo.ActorRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class ActorController {
     @Autowired
     private ActorRepo actorRepo;
-
 
     @PostMapping("/actors")
     //public void create(@PathVariable long id, @RequestBody Actor actor) {
     public void create(@RequestBody Actor actor) {
         actorRepo.save(actor);
         ResponseEntity.ok().build();
+    }
 
+    @GetMapping(value = "/actors")
+    public ResponseEntity<List<Actor>> read() {
+        List<Actor> actor = actorRepo.findAll();
+        return !actor.isEmpty()
+                ? new ResponseEntity<>(actor, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
+    @GetMapping(value = "/actors/{id}")
+    public ResponseEntity<Actor> read(@PathVariable(name = "id") Long id) {
+        Actor actor = actorRepo.findById(id).orElse(null);
+
+        return actor != null
+                ? new ResponseEntity<>(actor, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
