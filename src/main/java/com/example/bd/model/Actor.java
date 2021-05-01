41f7c437121b456239e807c49cbac068f050e0cc
Yulia <kjd2001@yandex.ru>
2021-05-01 21:57:29 +0300

    что то

--- src/main/java/com/example/bd/model/Actor.java
+++ src/main/java/com/example/bd/model/Actor.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Table(name = "actors")
 public class Actor implements Serializable {
     @Id
-    //@GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
     @Column(name = "first_NameA")
